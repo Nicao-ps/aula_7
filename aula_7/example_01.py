69fcf8ca098ccf4405ca71259ecd8d8216c9e5d7
Nicao-ps <nps.guimaraes@gmail.com>
2025-04-29 20:40:52 -0300

    modifica example_01.py

--- aula_7/example_01.py
+++ aula_7/example_01.py
@@ -1,1 +1,33 @@
- 
+def greetings():
+    print('Olá mundo!')
+
+
+greetings()
+
+
+def printline():
+    print(30*"=")
+
+
+printline()
+print("Título com moldura")
+printline()
+
+
+def sum(x, y):
+    s = x + y
+    return s
+
+    print(s)
+
+
+s = sum(4, 5)
+print(f'O valor da variável soma é {s}')
+
+
+for i in range(3):
+    number_1 = int(input('Informe o número 1: '))
+    number_2 = int(input('Informe o número 2: '))
+    
+    sum_numbers = sum(number_1, number_2)
+    print(sum_numbers)
