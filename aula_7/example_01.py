33ab80174b5f02d9589591ef796d4be456079a5c
Nicao-ps <nps.guimaraes@gmail.com>
2025-05-06 19:29:01 -0300

    modifica novamente example_01.py

--- aula_7/example_01.py
+++ aula_7/example_01.py
@@ -15,10 +15,9 @@ printline()
 
 
 def sum_x(x, y):
-    s = x + y
-    return s
-
-    print(s)
+    suum = x + y
+    print(int(suum))
+    return suum
 
 
 s = sum_x(4, 5)
@@ -26,8 +25,8 @@ print(f'O valor da variável sum é {s}')
 
 
 for i in range(3):
-    number_1 = int(input('Informe o número 1: '))
-    number_2 = int(input('Informe o número 2: '))
+    number1 = int(input('Informe o número 1: '))
+    number2 = int(input('Informe o número 2: '))
 
-    sum_numbers = sum(number_1, number_2)
+    sum_numbers = sum(number1, number2)
     print(sum_numbers)
